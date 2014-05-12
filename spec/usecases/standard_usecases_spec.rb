beafd3b816dc7b0782dfc7b73436636881ea7ee6
Andi Altendorfer <andi@iboard.cc>
2014-05-12 21:08:19 +0200

    Improve spec descriptions

--- spec/usecases/standard_usecases_spec.rb
+++ spec/usecases/standard_usecases_spec.rb
@@ -4,14 +4,13 @@ describe "Standard Application Usecases" do
 
   Given(:app) { ApplicationFactory.app( BaseApplication.instance ) }
 
-  describe "PING" do
+  describe "Base class Usecase handles - PING" do
     When(:request) { app.build_request( self, :ping, "pong" ) }
     When(:response){ app.handle_request(request) }
     Then {  expect( response.message ).to eq("pong") }
   end
 
-
-  describe "GETTIME" do
+  describe "Built in usecase GetTimeUsecase() - GETTIME" do
     When(:request) { ApplicationProtocol::Request.new( self, :gettime ) }
     When(:response){ app.handle_request(request) do |request| GetTimeUsecase.new( request ) end }
     Then {  expect( response.object ).to be_a(Time) }
